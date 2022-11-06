89426214cced92492e6d62a329f2c83409b09e0d
cauri <dev.aurich@gmail.com>
2022-11-06 10:20:45 +0100

    Add button inputs

--- app.js
+++ app.js
@@ -1,50 +1,110 @@
+const POINTS_NEEDED = 5
+let playerScore = 0
+let computerScore = 0
+
+const buttons = document.querySelectorAll('button')
+buttons.forEach((b) => b.addEventListener('click', (e) => {
+    const playerChoice = e.target.getAttribute('data-value')
+    const computerChoice = getComputerChoice()
+
+    const result = playRound(playerChoice, computerChoice)
+    show(result, playerChoice, computerChoice)
+    updateScore(result)
+    showScore()
+
+    if (somebodyWon()) {
+       announceWinner()
+       resetScore()
+    }
+}))
+
 function getComputerChoice() {
     const random = Math.floor(Math.random() * 3)
 
     switch (random) {
-        case 0: return "rock"
-        case 1: return "paper"
-        case 2: return "scissors"
+        case 0: return "Rock"
+        case 1: return "Paper"
+        case 2: return "Scissors"
         default: throw new Error('Invalid computer choice: ' + random)
     }
 }
 
 function playRound(playerChoice, computerChoice) {
     if (playerChoice === computerChoice) {
-        return "It's a draw!"
+        return "draw"
     }
 
-    if (playerChoice === "rock") {
-        if (computerChoice === "paper") {
-            return "You Lose! Paper beats Rock"
+    if (playerChoice === "Rock") {
+        if (computerChoice === "Paper") {
+            return "loss"
         } else {
-            return "You win! Rock beats Scissors"
+            return "win"
         }
     }
 
-    if (playerChoice === "paper") {
-        if (computerChoice === "scissors") {
-            return "You Lose! Scissors beats Paper"
+    if (playerChoice === "Paper") {
+        if (computerChoice === "Scissors") {
+            return "loss"
         } else {
-            return "You win! Paper beats Rock"
+            return "win"
         }
     }
 
-    if (playerChoice === "scissors") {
-        if (computerChoice === "rock") {
-            return "You lose! Rock beats Scissors"
+    if (playerChoice === "Scissors") {
+        if (computerChoice === "Rock") {
+            return "loss"
         } else {
-            return "You win! Scissors beats Paper"
+            return "win"
         }
     }
 }
 
-function playGame() {
-    for (let i = 0; i < 5; i++) {
-        const playerChoice = prompt("Rock, Paper, Scissors!").toLowerCase()
-        const computerChoice = getComputerChoice()
-        console.log(playRound(playerChoice, computerChoice))
+function show(result, playerChoice, computerChoice) {
+    switch (result) {
+        case "draw":
+            console.log("It's a draw!")
+            break;
+        case "win":
+            console.log(`You win! ${playerChoice} beats ${computerChoice}`)
+            break;
+        case "loss":
+            console.log(`You lose! ${computerChoice} beats ${playerChoice}`)
+            break;
+        default:
+            throw new Error('Unexpected result: ' + result)
     }
 }
 
-playGame()
+function updateScore(result) {
+    switch (result) {
+        case "win":
+            playerScore++;
+            break;
+        case "loss":
+            computerScore++;
+        default:
+            // tie, no update
+            break;
+    }
+}
+
+function showScore() {
+    console.log(`Player - ${playerScore} : ${computerScore} - Computer`)
+}
+
+function somebodyWon() {
+    return playerScore === POINTS_NEEDED || computerScore === POINTS_NEEDED
+}
+
+function announceWinner() {
+    if (playerScore > computerScore) {
+        console.log("You won the Game!")
+    } else {
+        console.log("Game Over")
+    }
+}
+
+function resetScore() {
+    playerScore = 0
+    computerScore = 0
+}
