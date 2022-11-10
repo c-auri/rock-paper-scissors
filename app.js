44e9dee510a6fcfaa6d466b473ec4c8065c655a5
c-auri <43008483+c-auri@users.noreply.github.com>
2022-11-10 10:52:16 +0100

    Add UI elements

--- app.js
+++ app.js
@@ -2,15 +2,20 @@ const ROUNDS_TO_WIN = 5
 let scorePlayer = 0
 let scoreComputer = 0
 
+const spanPlayerScore = document.querySelector('#score-player')
+const spanComputerScore = document.querySelector('#score-computer')
+const pRoundResult = document.querySelector('#round-result')
+const pGameResult = document.querySelector('#game-result')
+const buttonContainer = document.querySelector('#button-container')
 const buttons = document.querySelectorAll("button")
 
 function getComputerChoice() {
     const random = Math.floor(Math.random() * 3)
 
     switch (random) {
-        case 0: return "rock"
-        case 1: return "paper"
-        case 2: return "scissors"
+        case 0: return "Rock"
+        case 1: return "Paper"
+        case 2: return "Scissors"
         default: throw new Error('Invalid computer choice: ' + random)
     }
 }
@@ -20,24 +25,24 @@ function playRound(playerChoice, computerChoice) {
         return "It's a draw!"
     }
 
-    if (playerChoice === "rock") {
-        if (computerChoice === "paper") {
+    if (playerChoice === "Rock") {
+        if (computerChoice === "Paper") {
             return "You Lose! Paper beats Rock"
         } else {
             return "You win! Rock beats Scissors"
         }
     }
 
-    if (playerChoice === "paper") {
-        if (computerChoice === "scissors") {
+    if (playerChoice === "Paper") {
+        if (computerChoice === "Scissors") {
             return "You Lose! Scissors beats Paper"
         } else {
             return "You win! Paper beats Rock"
         }
     }
 
-    if (playerChoice === "scissors") {
-        if (computerChoice === "rock") {
+    if (playerChoice === "Scissors") {
+        if (computerChoice === "Rock") {
             return "You lose! Rock beats Scissors"
         } else {
             return "You win! Scissors beats Paper"
@@ -56,22 +61,51 @@ async function playGame() {
 
             if (roundResult.includes("win")) {
                 scorePlayer++
-                console.log("Player wins round")
-            } else if (roundResult.includes("Lose")) {
-                scoreComputer++
-                console.log("Computer wins round")
-            } else {
-                console.log("Draw!")
             }
 
-            console.log(`Player ${scorePlayer} : ${scoreComputer} Computer`)
+            if (roundResult.includes("Lose")) {
+                scoreComputer++
+            }
+
+            show(roundResult)
         })
     }
 
-        const winner = scorePlayer > scoreComputer ? "Player" : "Computer"
-        console.log(winner + " wins game!")
+        showGameResult()
         scorePlayer = 0
         scoreComputer = 0
 }
 
- (async function() { while (true) { await playGame() }})()
+function show(roundResult) {
+    spanPlayerScore.innerText = scorePlayer
+    spanComputerScore.innerText = scoreComputer
+    pRoundResult.textContent = roundResult
+}
+
+function showGameResult() {
+    if (scorePlayer > scoreComputer) {
+        pGameResult.innerText = "You Won!"
+    } else {
+        pGameResult.innerText = "Game Over"
+        toBlackScreen()
+    }
+
+    toggleVisibility(buttonContainer, pRoundResult, pGameResult)
+}
+
+function toBlackScreen() {
+    document.body.classList.remove('light')
+    document.body.classList.add('dark')
+}
+
+function toggleVisibility(...elements) {
+    for (const element of elements) {
+        if (element.getAttribute("data-visible") === "false") {
+            element.setAttribute("data-visible", "true")
+        } else {
+            element.setAttribute("data-visible", "false")
+        }
+    }
+}
+
+(async function() { while (true) { await playGame() }})()
